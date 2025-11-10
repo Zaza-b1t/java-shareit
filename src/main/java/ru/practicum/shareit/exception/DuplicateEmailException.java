f6d357b5c0c9ddf3e8362911ac060879e4cfdc02
Zaza <ilyatyuleneff@yandex.ru>
2025-11-10 08:48:32 +0300

    CheckStyle

--- src/main/java/ru/practicum/shareit/exception/DuplicateEmailException.java
+++ src/main/java/ru/practicum/shareit/exception/DuplicateEmailException.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.exception;
 
 import org.springframework.http.HttpStatus;
-import ru.practicum.shareit.exception.AbstractDtoException;
 
 public class DuplicateEmailException extends AbstractDtoException {
     public DuplicateEmailException(String message) {
