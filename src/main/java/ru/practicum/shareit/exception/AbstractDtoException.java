4fc047a38864592d548629498673a1c5fd437414
Zaza <ilyatyuleneff@yandex.ru>
2025-12-12 01:16:30 +0300

    Выполнено ФЗ16

--- src/main/java/ru/practicum/shareit/exception/AbstractDtoException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public abstract class AbstractDtoException extends RuntimeException {
-
-    public AbstractDtoException(String message) {
-        super(message);
-    }
-}
